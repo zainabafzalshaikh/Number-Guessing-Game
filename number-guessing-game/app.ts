7dea6a835dea97b7a956528fbd3752f8a9cc9b41
zainabafzalshaikh <161166927+zainabafzalshaikh@users.noreply.github.com>
2024-04-04 16:15:43 -0700

    Add files via upload

---
+++ number-guessing-game/app.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answers = await inquirer.prompt([
+   {
+       name: "userGuesedNumber",
+        type: "number",
+        message: "please guess a number between 1-6: ",
+
+    },
+])
+if(answers.userGuesedNumber === randomNumber){
+   console.log("congratulations! you guessed a right number");
+}
+else{
+    console.log("sorry! you guessed a wrong number");
+}
